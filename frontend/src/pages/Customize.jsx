ba322d7227c3964275b4daa34410b36bc0a1ec9e
Parth  Babariya <167310072+GitParth7777@users.noreply.github.com>
2025-09-03 23:18:19 +0530

    Update Customize.jsx

--- frontend/src/pages/Customize.jsx
+++ frontend/src/pages/Customize.jsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import { useContext } from 'react'
-import Card from '../components/card'
+import Card from '../components/Card.jsx'
 import image1 from "../assets/image1.png"
 import image2 from "../assets/image2.jpg"
 import image3 from "../assets/authBg.png"
