b01cd401628abaff4c43bcd3155eee7c71bc7cdd
Parth  Babariya <167310072+GitParth7777@users.noreply.github.com>
2025-09-03 23:10:18 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import UserContex from './contex/userContex.jsx'
+import UserContex from './contex/UserContex.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <UserContex>
